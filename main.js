e45abd495d5e95fc640182522d27ec494f44e4e5
Jimee <kjm12081@gmail.com>
2021-07-08 11:47:34 +1000

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-// 스크롤링 
-// navbar 의 height 만큼 스크롤링 되면  효과부여
 'use strict';
 
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {;
+
+
+document.addEventListener('scroll', () => {
   if(window.scrollY > navbarHeight) {
     navbar.classList.add('naver--dark');
   }else {
     navbar.classList.remove('naver--dark');
   };
+});
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  // navbar data-link 부분 외에 다른곳을 클릭해도 아무것도 출력되지 않음 
+  if(link == null) {
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"});
 });
